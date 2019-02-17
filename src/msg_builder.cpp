d0b4b0bf8bb5bf02aea34d99a7edf930fcb40b2a
Gregory Meyer <gregjm@umich.edu>
2019-02-17 14:03:41 -0500

    fully implement MsgBuilder, document impl

--- src/msg_builder.cpp
+++ src/msg_builder.cpp
@@ -26,8 +26,18 @@
 
 #include <srm/msg.h>
 
+#include <string_view>
+
 namespace srm {
 
+/**
+ *  @param minimum_size The minimum number of words to allocate.
+ *                      The allocated block will round this up to
+ *                      the nearest multiple of 16 words (128
+ *                      bytes). Must be positive.
+ *  @returns A segment of memory that is cache-aligned and a
+ *           multiple of the cache line size in length.
+ */
 kj::ArrayPtr<capnp::word> MsgBuilder::allocateSegment(capnp::uint minimum_size) {
     if (minimum_size % 128 != 0) {
         minimum_size += (128 - minimum_size % 128);
@@ -38,35 +48,64 @@ kj::ArrayPtr<capnp::word> MsgBuilder::allocateSegment(capnp::uint minimum_size) 
     return {arr.data(), arr.size()};
 }
 
+static const SrmMsgBuilderVtbl& get_vtbl() noexcept;
+
+/**
+ *  @returns An SrmMsgBuilder suitable for use by publishing
+ *           subroutines. Its lifetime is tied to this MsgBuilder.
+ */
+SrmMsgBuilder MsgBuilder::as_builder() noexcept {
+    return {this, &get_vtbl()};
+}
+
+static int allocate_segment_entry(void *impl_ptr, SrmMsgSegment *segment) noexcept;
+
+static SrmStrView err_to_str(int err) noexcept;
+
 static const SrmMsgBuilderVtbl& get_vtbl() noexcept {
     static const SrmMsgBuilderVtbl vtbl = {
-        [](void *impl_ptr, SrmMsgSegment *segment) -> int {
-            auto &builder = *static_cast<MsgBuilder*>(impl_ptr);
-
-            try {
-                auto allocated =
-                    builder.allocateSegment(static_cast<capnp::uint>(segment->len));
-
-                segment->data = reinterpret_cast<SrmWord*>(allocated.begin());
-                segment->len = static_cast<SrmIndex>(allocated.end() - allocated.begin());
-
-                return 0;
-            } catch (std::bad_alloc) {
-                return 1;
-            } catch (...) {
-                return 2;
-            }
-        },
-        [](int) -> SrmStrView {
-            return {nullptr, 0};
-        }
+        allocate_segment_entry,
+        err_to_str
     };
 
     return vtbl;
 }
 
-SrmMsgBuilder MsgBuilder::as_builder() noexcept {
-    return {this, &get_vtbl()};
+static int allocate_segment_entry(void *impl_ptr, SrmMsgSegment *segment) noexcept {
+    assert(impl_ptr);
+    assert(segment);
+    assert(segment->len > 0);
+
+    auto &builder = *static_cast<MsgBuilder*>(impl_ptr);
+
+    try {
+        auto allocated = builder.allocateSegment(static_cast<capnp::uint>(segment->len));
+
+        segment->data = reinterpret_cast<SrmWord*>(allocated.begin());
+        segment->len = static_cast<SrmIndex>(allocated.end() - allocated.begin());
+
+        return 0;
+    } catch (const std::bad_alloc&) {
+        return 1;
+    } catch (...) {
+        return 2;
+    }
+}
+
+static SrmStrView make_str_view(std::string_view sv);
+
+static SrmStrView err_to_str(int err) noexcept {
+    switch (err) {
+    case 0: return make_str_view("ok");
+    case 1: return make_str_view("out of memory");
+    case 2: return make_str_view("unknown error");
+    }
+
+    return {nullptr, 0};
+}
+
+static SrmStrView make_str_view(std::string_view sv) {
+    return {sv.data(), static_cast<SrmIndex>(sv.size())};
 }
 
 } // namespace srm
